c6f299d53de265ab3a48445432404fc617ed231f
chenxiongyu <chenxiongyu@guoshanggroup.com>
2017-09-29 15:57:34 +0800

    fix route(index) bug

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
+                    <span>Copyright © <a href="{{ route('post.index') }}">{{ $site_title or '' }}</a></span> |
                     <span>Powered by <a href="https://github.com/obdobriel/Xblog">Xblog</a> </span>|
                     <a href="{{ url('feed.xml') }}" rel="feed" type="application/rss+xml"
                        title="Feed"><img width="13"
